5e2d0013fab1a563932bd684da747d49cf49fc5f
Josh <joshconstantinelim@gmail.com>
2019-04-26 19:27:19 +1000

    Setting up structure of project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB85Q2w9sFJg3m_Jb0qHbzPIAhPEFrwIj0",
+  authDomain: "finished-jcl.firebaseapp.com",
+  databaseURL: "https://finished-jcl.firebaseio.com",
+  projectId: "finished-jcl",
+  storageBucket: "finished-jcl.appspot.com",
+  messagingSenderId: "113361144260"
+};
+firebase.initializeApp(config);
