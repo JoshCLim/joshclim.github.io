b98ab7b806f819250e2c30fef8fa5569376d6386
Josh <joshconstantinelim@gmail.com>
2019-04-29 10:09:35 +1000

    begin a create game box

---
+++ finished/js/finished.js
@@ -0,0 +1,20 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB85Q2w9sFJg3m_Jb0qHbzPIAhPEFrwIj0",
+    authDomain: "finished-jcl.firebaseapp.com",
+    databaseURL: "https://finished-jcl.firebaseio.com",
+    projectId: "finished-jcl",
+    storageBucket: "finished-jcl.appspot.com",
+    messagingSenderId: "113361144260"
+};
+firebase.initializeApp(config);
+
+$('.create-game-button').on('click', function() {
+    $('.home-page-container').fadeOut();
+    $('.create-game-container').fadeIn();
+});
+
+$('.create-game-back').on('click', function() {
+    $('.create-game-container').fadeOut();
+    $('.home-page-container').fadeIn();
+});
