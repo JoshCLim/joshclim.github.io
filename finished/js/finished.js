8fadcf57850185b787ed858248b24632b89a341f
Josh <joshconstantinelim@gmail.com>
2019-04-30 16:02:21 +1000

    join game, cheat codes, display username

--- finished/js/finished.js
+++ finished/js/finished.js
@@ -10,6 +10,32 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+// cheat codes!!
+$('.cheat-input').on('keypress', function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        //alert('You pressed a "enter" key in textbox');
+        var cheatCode = $('.cheat-input').val();
+
+        // cheat codes here
+        if (cheatCode == 'pregame') {
+            $('.home-page-container').fadeOut();
+            $('.create-game-container').fadeOut();
+            $('.join-game-container').fadeOut();
+            $('.pregame-container').fadeIn(); // go to pregame container
+        }
+    }
+});
+
+$(window).on("beforeunload", function() {
+    var playersReference = database.ref('/games/' + code + '/players/');
+    playersReference.child(username).remove();
+});
+
+function updateUsername() {
+    $('.username').text(username);
+}
+
 function generateGameCode() {
     var a = 0;
     while (String(a).length != 7) {
@@ -26,7 +52,10 @@ function loadGameRoom() {
     code = generateGameCode();
     $('.seven-digit-game-code').text(code);
     var reference = database.ref('/games/').child(code).child('players').child(username);
-    reference.set({username: username});
+    reference.set({
+        username: username,
+        host:true // make game room creator host
+    });
     updatePlayersDatabase(); // update the player database only after variable 'code' has been defined
 }
 
@@ -35,13 +64,26 @@ function updatePlayersDatabase() {
     console.log(code);
     playersReference.on('value', function(r) {
         var allPlayers = r.val();
-        console.log(allPlayers);
+        //console.log(allPlayers);
+        $('.players-list ul').empty();
         for (var item in allPlayers) {
             var q = allPlayers[item].username;
             console.log(q)
             $('.players-list ul').append('<li class="players">' + q + '</li>');
         }
     });
+}
+
+function joinGameRoom() {
+    $('.join-game-container').fadeOut();
+    $('.pregame-container').fadeIn();
+    $('.seven-digit-game-code').text(code);
+    var reference = database.ref('/games/').child(code).child('players').child(username);
+    reference.set({
+        username: username,
+        host: false // give joiner host 'false'
+    });
+    updatePlayersDatabase();
 }
 
 $('.create-game-button').on('click', function() {
@@ -54,20 +96,57 @@ $('.create-game-back').on('click', function() {
     $('.home-page-container').fadeIn();
 });
 
-var enter_name_input = $('.enter-name');
-enter_name_input.on('change', function() {
-    if (enter_name_input.val() == '') {
+$('.join-game-button').on('click', function() {
+    $('.home-page-container').fadeOut();
+    $('.join-game-container').fadeIn().css('display', 'flex');
+});
+
+$('.join-game-back').on('click', function() {
+    $('.join-game-container').fadeOut();
+    $('.home-page-container').fadeIn();
+});
+
+var create_enter_name_input = $('.create-enter-name');
+create_enter_name_input.on('change', function() {
+    if (create_enter_name_input.val() == '') {
         $('.create-game-next').fadeOut(500);
     } else {
         $('.create-game-next').fadeIn(500);
     }
 });
 
+var join_enter_name_input = $('.join-enter-name');
+join_enter_name_input.on('change', function() {
+    if (join_enter_name_input.val() == '') {
+        $('.join-game-next').fadeOut(500);
+    } else {
+        $('.join-game-next').fadeIn(500);
+    }
+});
+
 var username;
 $('.create-game-next').on('click', function() {
-    username = $('.enter-name').val();
+    username = $('.create-enter-name').val();
     //console.log(username);
     $('.create-game-container').fadeOut();
     $('.pregame-container').fadeIn();
+    updateUsername();
     loadGameRoom();
+});
+
+$('.join-game-next').on('click', function() {
+    username = $('.join-enter-name').val();
+    code = $('.join-enter-code').val();
+    var checkReference = database.ref('/games/' + code + '/players/');
+    checkReference.once('value').then(function(r) {
+        var results = r.val();
+        if (results != null) {
+            updateUsername();
+            joinGameRoom();
+        } else {
+            alert('Invalid game code!!')
+        }
+    })
+    //console.log(username);
+    //console.log(code);
 });
