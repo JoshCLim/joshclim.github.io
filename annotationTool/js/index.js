d6f74956dfbe35bed9db95b9392eea488268f04b
Josh <joshconstantinelim@gmail.com>
2021-04-21 23:26:22 +1000

    adding comment functionality

--- annotationTool/js/index.js
+++ annotationTool/js/index.js
@@ -16,7 +16,7 @@ var stuffToPushToDatabase = [];
 var wordID;
 var currentLinkCode = '';
 
-var currentText = "a12-614";
+var currentText = 'a12-681';
 
 var yellow = '#f6f6eb';
 var blue = '#d5d6ea';
@@ -27,10 +27,30 @@ var purple = '#f3ddf2';
 var none = 'rgba(255,255,255,0)'
 
 var codes = { // text code: ['name of text', line number start, line number end],
-	'a12-614': ['Aeneid 12.614-680',614,680],
-	'a12-681': ['Aeneid 12.681-759',681,759],
-	'c2': ['Catullus 2',1,12],
-	'c7': ['Catullus 7',1,15]
+	'a12-614': ['Aeneid 12.614-696',614,696], // https://www.poetryintranslation.com/PITBR/Latin/VirgilAeneidXII.php
+	'a12-697': ['Aeneid 12.697-765',697,765],
+	'a12-766': ['Aeneid 12.766-790',766,790],
+	'a12-791': ['Aeneid 12.791-842',791,842],
+	'a12-843': ['Aeneid 12.843-886',843,886],
+	'a12-887': ['Aeneid 12.887-952',887,952],
+	'c2': ['Catullus 2',1,13],
+	'c13': ['Catullus 13',1,14],
+	'c35': ['Catullus 35',1,18],
+	'c40': ['Catullus 40',1,8],
+	'c51': ['Catullus 51',1,16],
+	'c62': ['Catullus 62',1,67],
+	'c67': ['Catullus 67',1,48],
+	'c70': ['Catullus 70',1,4],
+	'c75': ['Catullus 75',1,4],
+	'c87': ['Catullus 87',1,4],
+	'c96': ['Catullus 96',1,6],
+	'c99': ['Catullus 99',1,16],
+	'c110': ['Catullus 110',1,8],
+	'h1-5': ['Horace 1.5',1,16],
+	'h1-13': ['Horace 1.13',1,20],
+	'h1-22': ['Horace 1.22',1,24],
+	'h3-26': ['Horace 3.26',1,12],
+	'h4-1': ['Horace 4.1',1,40]
 }
 
 // creating transition delays for colour bars
@@ -47,14 +67,34 @@ var transitionDelayStyleSheet = document.createElement("style");
 transitionDelayStyleSheet.type = "text/css";
 transitionDelayStyleSheet.title = 'transitionDelayStyles';*/
 
+// handlebars set up for comments template
+var commentsInfo = document.getElementById("comment-template").innerHTML; // https://www.youtube.com/watch?v=4HuAnM6b2d8
+var commentsTemplate = Handlebars.compile(commentsInfo);
+
+
 
 
 $(document).ready(function() {
 
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+
+
+
+
+
 
 // add labels to selection menu
 for (var key in codes) {
-	$(".selection-bar").append('<label for="' + key + '"><input type="radio" name="text" id="' + key + '" value="'+ codes[key][0] +'"><span>'+ codes[key][0] +'</span></label>')
+	$(".selection-container").append('<label for="' + key + '"><input type="radio" name="text" id="' + key + '" value="'+ codes[key][0] +'"><span>'+ codes[key][0] +'</span></label>')
 }
 // <label for="a12-614"><input type="radio" name="text" id="a12-614" value="Aeneid 12.614-680"><span>Aeneid 12.614-680</span></label>
 // <label for="a12-681"><input type="radio" name="text" id="a12-681" value="Aeneid 12.681-759"><span>Aeneid 12.681-759</span></label>
@@ -113,26 +153,30 @@ $(".selection-bar label[for=a12-614]").click();
 // get words and reformat
 	//var stuff = $().split('\n').join(' <br>').split('  ').join(' ').split(' ');
 $('#text-submit').on('click', function() {
-	var words = $("#text-input").val().split('\n').join(' <br>').split(' ');
+	if (currentText != '') {
+		var words = $("#text-input").val().split('\n').join(' <br>').split(' ');
 
-	$(".text-div").empty();
+		$(".text-div").empty();
 
-	for (var q in words) {
-		words[q] = words[q].replace(/\s/g,''); // https://www.techiedelight.com/remove-whitespaces-string-javascript/
-		//console.log(words[q])
-		//$(".text-div").append("<span>" + words[q] + "</span> ")
+		for (var q in words) {
+			words[q] = words[q].replace(/\s/g,''); // https://www.techiedelight.com/remove-whitespaces-string-javascript/
+			//console.log(words[q])
+			//$(".text-div").append("<span>" + words[q] + "</span> ")
 
-		stuffToPushToDatabase.push({
-			name:words[q],
-			colour:'none',
-			linkCode:''
-		});
+			stuffToPushToDatabase.push({
+				name:words[q],
+				colour:'none',
+				linkCode:''
+			});
+		}
+		//console.log(stuffToPushToDatabase);
+		database.ref('/texts/').child(currentText).remove();
+		for (var i in stuffToPushToDatabase) {
+	      database.ref('/translationText/').child(currentText).push(stuffToPushToDatabase[i]);
+	    }
+	} else {
+		alert("ERROR")
 	}
-	//console.log(stuffToPushToDatabase);
-	database.ref('/texts/').child(currentText).remove();
-	for (var i in stuffToPushToDatabase) {
-      database.ref('/translationText/').child(currentText).push(stuffToPushToDatabase[i]);
-    }
 });
 
 function updateTextDiv() {
@@ -140,19 +184,48 @@ database.ref('/translationText/').child(currentText).on('value', function(result
 	var allResults = results.val();
 
 	$('.text-div').empty();
+	$('.annotations-div').empty();
 
+	var commentsList = [];
+
+	// display all words
 	for (var i in allResults) {
+		// display all words
 		$('.text-div').append('<span data-link="'+ allResults[i].linkCode + '" data-id="' + i + '" style="background-color:var(--' + allResults[i].colour + ') !important;">' + allResults[i].name + '</span> ')
+
+		// display all comments
+		if (allResults[i].comments != undefined) {
+			for (var q in allResults[i].comments) {
+				//console.log(allResults[i].comments[q].content)
+
+				commentsList.push({comment: allResults[i].comments[q].content});				
+			}
+			//console.log(commentsList)
+			var commentsData = commentsTemplate({
+				word: allResults[i].name,
+				comments: commentsList,
+				wordID: i
+			});
+			$('.annotations-div').append(commentsData);
+		}
+		commentsList = []; // clear the comments list again
 	}
 
- 	// if a word is clicked on, apply the selected class
-	$(".text-div > span").on('click', function() {
+
+	$(".text-div > span").on('click', function() { // if a word is clicked on...
 		$('.selected').removeClass('selected');
 		$(this).addClass('selected');
 
 		wordID = $(this).data('id');
 
-		if (currentLinkCode != '') {
+		if ($(this).data('link') != '') {  // if the selected word is linked, highlight other linked words
+			$(".text-div > span").removeClass('link-highlight');
+			$('[data-link="' + $(this).data('link') + '"').addClass('link-highlight')
+		} else {
+			$(".text-div > span").removeClass('link-highlight');
+		}
+
+		if (currentLinkCode != '') { // if there is an active link code, apply it to the clicked element
 			database.ref('/translationText/').child(currentText).child(wordID).update({
 		          linkCode:currentLinkCode
 		    });
@@ -161,13 +234,20 @@ database.ref('/translationText/').child(currentText).on('value', function(result
 		currentLinkCode = ''; // remove any active link code
 		$('.link-button').removeClass('link-button-processing'); // stop link button from being red
 	});
+
+	// when you hover over a word, (1)show linked words
 	$(".text-div > span").on('mouseover', function() {
 		if ($(this).data('link') != '') {
+			$(".text-div > span").removeClass('link-highlight');
 			$('[data-link="' + $(this).data('link') + '"').addClass('link-highlight')
 		}
 	});
 	$(".text-div > span").on('mouseleave', function() {
 		$(".text-div > span").removeClass('link-highlight');
+		if ($(".selected").data('link') != '') {  // if the selected word is linked, highlight other linked words
+			$(".text-div > span").removeClass('link-highlight');
+			$('[data-link="' + $(".selected").data('link') + '"').addClass('link-highlight')
+		}
 	});
 
 	//$(".text-div span:nth-child(1)").click(); // click on the first word
@@ -200,6 +280,7 @@ function getSelected() {
 	return false;
 }
 
+/*
 // on highlight, return selection and show toolbar
 $('.text-div').mouseup(function(event) {
 	var selection = getSelected();
@@ -243,6 +324,7 @@ $('.text-div').mouseup(function(event) {
 		$(".tools-bar").css('display', 'none');
 	}
 });
+*/
 
 // darken colour bars on mouse down
 $(".colour-bar > div").on('mousedown', function() {
@@ -281,30 +363,54 @@ $(document).keydown(function(e) {
 		case ("ArrowRight"):
 			$('.selected').next().click();
 			break;
+
 		case ("ArrowLeft"):
 			$('.selected').prev().click();
 			break;
+
 		case ("1"):
 			highlightWord('yellow');
 			break;
+
 		case ("2"):
 			highlightWord('green');
 			break;
+
 		case ("3"):
 			highlightWord('blue');
 			break;
+
 		case ("4"):
 			highlightWord('purple');
 			break;
+
 		case ("5"):
 			highlightWord('pink');
 			break;
+
 		case ("6"):
 			highlightWord('orange');
 			break;
+
 		case ("0"):
 			highlightWord('none');
 			break;
+
+		case ("Enter"):
+			if ($(".comment-button").hasClass("comment-button-processing")) {
+				var comment = $(".comments-input").text();
+				//console.log(comment)
+				if (comment != '') {
+					database.ref('/translationText/').child(currentText).child(wordID).child('comments').push({
+						content: comment
+					});
+				}
+
+				resetComment();
+			}
+
+			break;
+
 		default:
 			break;
 	}
@@ -322,8 +428,8 @@ $(".colour-bar-colour").on('mouseleave', function() {
 
 // highlight words on click
 function highlightWord(colour) {
-	//var currentWord = $('.selected').text();
-	//$(".selected").attr('style', 'background-color:var(--' + colour + ') !important;')
+				//var currentWord = $('.selected').text();
+				//$(".selected").attr('style', 'background-color:var(--' + colour + ') !important;')
 	database.ref('/translationText/').child(currentText).child(wordID).update({
           colour:colour
     });
@@ -337,24 +443,54 @@ $(".colour-bar-colour:nth-child(6)").on('click', function() {highlightWord('oran
 
 
 $(".link-button").on('click', function() {
-	if ($(".selected")) {
+	if ($(".selected").length != 0) {
 		if (currentLinkCode == '') {
 			if ($(".selected").data('link') == "") {
 				var newCode = Date.now();
 				database.ref('/translationText/').child(currentText).child(wordID).update({
-			          linkCode:newCode
+			    	linkCode:newCode
 			    });
 			    currentLinkCode = newCode;
 			} else {
 				currentLinkCode = $(".selected").data('link');
 			}
 			$('.link-button').addClass('link-button-processing');
-		} else {
+		} else { // if there is an active link code and the button is pressed again, everything is reset
 			$('.link-button').removeClass('link-button-processing');
+			currentLinkCode = ''; 
+			database.ref('/translationText/').child(currentText).child(wordID).update({
+				linkCode:''
+			});
+		}
+	}
+});
+
+function resetComment() {
+	$(".comments-input").text('');
+
+	$(".comments-bar").animate({"min-width":"0px"},200);
+	$(".comments-bar").animate({"padding":"5px 0px"},40);
+	$(".comments-input").animate({"margin-left":"0px"},60);
+
+	$(".comment-button").removeClass("comment-button-processing");
+}
+
+$(".comment-button").on("click", function() {
+	if ($(".selected").length != 0) {
+		if ($(".comment-button").hasClass("comment-button-processing")) {
+			resetComment();
+		} else {
+
+			$(".comments-bar").animate({"min-width":"100px"},200);
+			$(".comments-bar").animate({"padding":"5px 20px"},40);
+			$(".comments-input").animate({"margin-left":"30px"},60);
+
+			$(".comment-button").addClass("comment-button-processing");
+
+			$(".comments-input").focus();
 		}
 	}
 });
 
 
 }); // end of document.ready function
-
