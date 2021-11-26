026d90474ccd5d3a0abdca29a12933b1dec5fe99
Josh <joshconstantinelim@gmail.com>
2021-11-26 18:30:17 +1100

    pokedex v1.0

---
+++ pokedex/script.js
@@ -0,0 +1,72 @@
+const pokemonSource = document.getElementById("pokemon-container-template").innerHTML;
+const pokemonTemplate = Handlebars.compile(pokemonSource);
+
+const pokemonContainer = document.getElementById("pokemon-container");
+
+const fetchData = () => { // fetch data from the pokemon API
+    const apiURL = "https://pokeapi.co/api/v2/pokemon/";
+
+    pokemonContainer.innerHTML = "";
+
+    for (let i = 1; i <= 150; i++) {
+        
+        let url = apiURL + i;
+
+        fetch(url).then(function(res) {
+            return res.json();
+        }).then(function(data) {
+            //console.log(data);
+
+            let pokemonData = {
+                id: i,
+                name: data.name,
+                pictureLink: data.sprites.front_default,
+                types: data.types.map(q => q.type.name).join(", "),
+                type: data.types.map(q => q.type.name)
+            }
+            //console.log(pokemonData); 
+
+            let pokemonHTML = pokemonTemplate(pokemonData);
+            pokemonContainer.innerHTML += pokemonHTML;
+        });
+    }    
+}
+
+fetchData();
+
+
+const stylesheet = window.document.styleSheets[0];
+
+const pokemonTypes = [
+    {type: "bug", colour: "#92BC2C"},
+    {type: "dark", colour: "#595761"},
+    {type: "dragon", colour: "#0C69C8"},
+    {type: "electric", colour: "#F2D94E"},
+    {type: "fairy", colour: "#EE90E6"},
+    {type: "fighting", colour: "#D3425F"},
+    {type: "fire", colour: "#FBA54C"},
+    {type: "flying", colour: "#A1BBEC"},
+    {type: "ghost", colour: "#5F6DBC"},
+    {type: "grass", colour: "#5FBD58"},
+    {type: "ground", colour: "#DA7C4D"},
+    {type: "ice", colour: "#75D0C1"},
+    {type: "normal", colour: "#A0A29F"},
+    {type: "poison", colour: "#B763CF"},
+    {type: "psychic", colour: "#FA8581"},
+    {type: "rock", colour: "#C9BB8A"},
+    {type: "steel", colour: "#5695A3"},
+    {type: "water", colour: "#539DDF"}
+]
+
+for (let p = 0; p < pokemonTypes.length; p++) {
+    let currentType = pokemonTypes[p].type;
+    let currentColour = pokemonTypes[p].colour;
+
+    let currentRule = 
+    `.types-icon-${currentType} { 
+        background-color: ${currentColour} !important; 
+        box-shadow: 0 0 5px ${currentColour} !important; 
+    }`;
+
+    stylesheet.insertRule(currentRule, stylesheet.cssRules.length);
+}
